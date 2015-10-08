ef30681e9b2d29e94837e8c55ce55f46a5aa63c6
Rex Feng <rexfeng@gmail.com>
2015-10-08 08:21:59 -0700

    add wrapper for Isolation.run

--- spec/isolation/command_runner_spec.rb
+++ spec/isolation/command_runner_spec.rb
@@ -2,20 +2,34 @@ require 'spec_helper'
 
 module Isolation; describe CommandRunner do
 
-  it 'runs commands that do not blow up' do
-    runner = Isolation::CommandRunner.new("ls")
-    runner.run
+  context '.new' do
 
-    expect(runner.success?).to eq(true)
-    expect(runner.error_message).to eq("")
+    it 'runs commands that do not blow up' do
+      runner = Isolation::CommandRunner.new("ls")
+      runner.run
+
+      expect(runner.success?).to eq(true)
+      expect(runner.error_message).to eq("")
+    end
+
+    it 'captures error messages for commands that blow up' do
+      runner = Isolation::CommandRunner.new("ls -z")
+      runner.run
+
+      expect(runner.success?).to eq(false)
+      expect(runner.error_message).to include "ls: illegal option"
+    end
   end
 
-  it 'captures error messages for commands that blow up' do
-    runner = Isolation::CommandRunner.new("ls -z")
-    runner.run
+  context '.run wrapper' do
 
-    expect(runner.success?).to eq(false)
-    expect(runner.error_message).to include "ls: illegal option"
+    it 'runs commands that do not blow up' do
+      runner = Isolation.run("ls")
+
+      expect(runner.success?).to eq(true)
+      expect(runner.error_message).to eq("")
+    end
+
   end
 
 end; end
