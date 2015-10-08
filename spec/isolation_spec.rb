e30a47fa4b347b42f01c48c76e0bebb410599d2d
MrPowers <matthewkevinpowers@gmail.com>
2015-10-08 09:36:51 -0500

    Update the README and add some specs

--- spec/isolation_spec.rb
+++ spec/isolation_spec.rb
@@ -1,15 +1,9 @@
 require 'spec_helper'
 
 describe Isolation do
+
   it 'has a version number' do
     expect(Isolation::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    runner = Isolation::CommandRunner.new("ls")
-    runner.run
-
-    expect(runner.success?).to eq(true)
-    expect(runner.error_message).to eq("")
-  end
 end
